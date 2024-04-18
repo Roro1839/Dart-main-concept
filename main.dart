6636ed16271624bd9feb78055b397986dad14816
Roro1839 <ronaldokipkirui90@gmail.com>
2024-04-18 15:08:18 +0300

    new

--- main.dart
+++ main.dart
@@ -1,22 +1,19 @@
-import 'dart.io'
+import 'dart:io';
 
-void main()
-{
-std.stdout.write("Enter the number:");
-String userInput = std.stdin.readLineSync();
-int number = int.tryParse(userInput);
+void main() {
+ print("Enter the number:");
+ String? userInput = stdin.readLineSync();
+ int? number = int.tryParse(userInput ?? '');
 
-switch(number){
-case (number > 10):
-print ("Your number is greater than 10");
-break;
-case (number < 10):
-print ("Your number is less than 10");
-break;
-case (number == 10):
-print ("Your number is equal to 10");
-break;
-default:
-print ("Value error");
-break;
+ if (number != null) {
+    if (number > 10) {
+      print("Your number is greater than 10");
+    } else if (number < 10) {
+      print("Your number is less than 10");
+    } else if (number == 10) {
+      print("Your number is equal to 10");
+    }
+ } else {
+    print("Value error");
+ }
 }
